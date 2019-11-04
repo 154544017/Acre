724990d7b9059625e97019c23ec1379f078f1f18
Greilfang <565222945@qq.com>
2019-11-04 11:18:15 +0800

    import package

--- src/Test/VisitorTest.java
+++ src/Test/VisitorTest.java
@@ -13,13 +13,14 @@ import Model.Rancher;
 import Model.Scene.PlantFarmScene;
 
 import Model.Goods.GoodsEnum;
+import Util.MyUtils;
 import org.junit.Test;
 
 public class VisitorTest {
     public static void main(String[] args) {
         //加载玩家信息
         System.out.println(MyUtils.getInfo("访问者模式测试"));
-        System.out.println(MyUtils.getInfo('accept 接收buryIn方法,实现访问对象不同操作不同'));
+        //System.out.println(MyUtils.getInfo("accept接收buryIn方法"));
         Rancher rancher = new Rancher("fp");
         //建立种植物的地方
         PlantFarmScene scene = (PlantFarmScene)new PlantFarmSceneBuilder("PlantFarm", rancher).construct();
